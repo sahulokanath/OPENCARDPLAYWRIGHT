6d7884210300ef1c1b284d847e8395db8b96f052
Lokanath <lokanathsahu600@gmail.com>
2026-04-23 22:28:37 +0530

    envsetup.sh

--- env_setup.bat
+++
@@ -1,4 +0,0 @@
-npm install
-npm install -D allure-playwright
-npm install -g allure-commandline --force
-npx playwright-install
---
+++ env_setup.sh
@@ -0,0 +1,3 @@
+npm install
+npm install -D allure-playwright
+npm install -g allure-commandline --force
